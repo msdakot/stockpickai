76659ad494153e29b440ecf6ac8f0f180c02bd94
msdakot <dhruvikothari@Dhruvis-MacBook-Pro.local>
2022-07-25 18:50:18 -0700

    charting

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.io as pio
 import seaborn as sb
 
 # loading local csv-file
-results_df=pd.read_csv('results_first.csv')
+top_picks_df=pd.read_csv('data/top_picks_information.csv')
+model_df=pd.read_csv('data/model-period-7-year-60-cycles.csv')
+chart_df = pd.read_csv('data/model-period-7-year-60-cycles.csv')
 
-df=pd.read_csv('AAPL_prices.csv')
-df['datadate']=pd.to_datetime(df['datadate'])
+#processing 
+top_picks_df['start_date']=pd.to_datetime(top_picks_df['start_date'])
+top_picks_df['month'] = top_picks_df['start_date'].dt.to_period('M')
 
 
 #showing file with streamlit
@@ -17,13 +22,37 @@ st.subheader("Stocks that would outperform S&P500")
 month_list = results_df['month'].unique().tolist()
 
 month_selector = st.selectbox(
-    "Select a month",
+    "Select a month period",
     month_list 
 )
-st.dataframe(results_df.head(5))
+
+col_to_print = {'month':'Selected Month Period',
+                'top_pick': 'StockPick Stocks',
+                'probability': 'Probability to outperform S&P',
+                'model_accuracy': 'StockPick Model Accuracy'}
+filter_pick_df = top_picks_df[top_picks_df['month'] == month_selector].copy().rename(columns=col_to_print )
+
+st.dataframe(filter_pick_df[list(col_to_print.keys())].head(5))
 
 # plot the csv-file
-fig = plt.figure(figsize=(20,5))
-sb.set(style="darkgrid")
-sb.lineplot(x=df['datadate'], y=df['prccd'],label='Apple')
-st.pyplot(fig)
+pio.templates.default = "plotly_white"
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=chart_df['date'], y=chart_df['Stocks Average Return'],
+                    mode='lines',
+                    line_color='#fe7062',
+                    name='StockPick Avearge Return'))
+
+fig.add_trace(go.Scatter(x=chart_df['date'], y=chart_df['S and P Average Return'],
+                    mode='lines',
+                    line_color='#456067',
+                    name='S&P Average Return'))
+
+fig.update_layout(legend=dict(
+    yanchor="top",
+    y=0.99,
+    xanchor="left",
+    x=0.01
+))
+
+fig.show()
